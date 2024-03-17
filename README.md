95e373d32495ad8f4ad28c3a76471303b414add6
Tomas Vince <tomas.vince@gmail.com>
2024-03-17 21:11:05 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # Text-2-Speech-using-ElevenLabs-API
+
+This application uses the OpenAI API to convert text to speech and save the audio file as mp3 to local disk.
+
+## Author
+
+Tomas Vince - Cloud Architect | AI Enthusiast
+Email: tomas.vince@gmail.com
+Linkedin: https://www.linkedin.com/in/tomasvince/
+
+## Features
+
+- Reads input text from a file named "text-for-conversion.txt".
+- Converts the input text to speech using the ElevenLabs API.
+- Saves the resulting audio to a dedicated file into "audio-outputs" directory.
+- The audio file is named in the format "speech-YYYY-MM-DD_HH-MM.mp3", where "YYYY-MM-DD_HH-MM" is the current date and time.
+
+## 1. Setup
+
+1. Create dedicated local folder on your computer for your project.
+2. Clone GIT repository into your folder
+3. Create virtual environment using following commands: `python3 -m venv venv` 
+4. Activate virtual environment using following commands: `source venv/bin/activate` 
+5. Install the required Python packages with `pip install -r requirements.txt`.
+6. Rename file '.env.EXAMPLE' to ".env", open renamed file and set up your OpenAI API key. **(This step is required)**
+7. Place the text you want to convert to speech in a file named "text-for-conversion.txt" and save. *(This step is required if you want to run text to speech conversion via CLI using dedicated python script tts-app.py)*
+8. To run text-2-speech converter tool in more convenient way (via web ui) take a look at section **3. Streamlit web framework** for more details how to run Streamlit web framework.
+9. Additional way (most convenient way) how to run converter tool is by running docker container. See **4. Run Text-2-Speech app as Docker container** section for technical details how to make it done.
+
+## 2. Usage
+
+Run the application with `python3 tts-app.py`.
+
+## 3. Streamlit web framework
+
+To create a Streamlit web application that incorporates the functionality of created Python script to convert text to an MP3 file, follow these steps. 
+
+*Note: This guide assumes you have Streamlit and the necessary libraries (like openai) installed in your environment.*
+
+This script is saved in a tts-using-stramlit.py file and can be run using Streamlit by navigating to the directory containing the script and running: 
+`streamlit run tts-app-streamlit.py`
+
+New browser tab will be opened with rendered web app:
+
+![text to speech converter UI](img/image.png)
+
+Text for translation can be copied into text field and then button **Convert to Speech** has to be submitted. After processing **Download** button appears and user can download mp3 file locally on its computer.
+
+## 4. Run Text-2-Speech app as Docker container
+
+To build Docker image Dockerfile has been defined. 
+Use following command to create docker image:
+
+`docker build -t text-2-speech-converter-image .`
+
+To run docker container from created docker image use following command:
+
+`docker run --name Text-2-Speech-Converter --env-file=.env -p 8501:8501 text-2-speech-converter-image`
+
+
+## Note
+
+This application suppresses DeprecationWarnings. This is a temporary solution and it's generally a good idea to update your code to use the new methods when they become available.
---
+++ tts-app.py
@@ -0,0 +1,141 @@
+# Description: This is a simple Python script that uses the ElevenLABS API to convert text to speech and save the audio file to disk.
+
+# Import the required libraries
+from pathlib import Path
+import requests
+import os
+import time
+import warnings
+from dotenv import load_dotenv
+from datetime import datetime
+from elevenlabs.client import ElevenLabs
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the current date and time for filename uniqueness
+now = datetime.now()
+date_string = now.strftime("%Y-%m-%d_%H-%M")
+
+# Suppress DeprecationWarning
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+# Create an instance of the ElevenLabs client with the API key
+elevenlabs_api_key = os.getenv('ELEVEN_API_KEY')
+client = ElevenLabs(api_key=elevenlabs_api_key)
+
+# Define the path to the text file
+text_file_path = Path(__file__).parent / "text-for-conversion.txt"
+
+# Initialize an empty list to hold the voice names for Streamlit dropdown menu options
+voice_names = []
+
+# List all available Elevenlabs voices and print their details.
+supported_voices = client.voices.get_all()
+# print("\n")
+# print("***************************************************")
+# print("Available Elevenlabs voices:")
+# print("***************************************************")
+# print("\n")
+#print(supported_voices) # Print the entire raw response
+
+# Iterate over each voice in the response and print the required details.
+for voice in supported_voices.voices:
+    gender = voice.labels.get('gender', 'Unknown')
+    voice_names.append(voice.name)
+    #print(f"Voice ID: {voice.voice_id}, Name: {voice.name}, Category: {voice.category},Gender: {gender}")
+print("\n")
+
+# End of the script which list all available Elevenlabs voices
+
+# Assuming voice_names is already populated with the names of available voices
+for index, name in enumerate(voice_names, start=1):
+    print(f"{index}: {name}")
+
+# Ask the user to select a voice for generating the speech audio
+user_choice = input("Please choose a voice by typing the number next to it: ")
+
+# Validate the user's choice
+try:
+    choice_index = int(user_choice) - 1  # Convert to zero-based index
+    if choice_index < 0 or choice_index >= len(voice_names):
+        raise ValueError("Choice out of range")
+except ValueError as e:
+    print("Invalid choice, please run the script again and select a valid number.")
+    exit()
+
+# Retrieve the selected voice name
+selected_voice_name = voice_names[choice_index]
+print("\n")
+print("*****************************************************")
+print(f"You have selected the following voice: {selected_voice_name}")
+print("*****************************************************")
+print("\n")
+
+##########################################
+# *** Beginning of Audio synthesis script 
+##########################################
+
+# Open the text file and read its content
+with open(text_file_path, 'r') as in_file:
+    input_text = in_file.read()
+
+
+# Generate the speech audio using Elevenlabs API from the provided text
+try:
+    response = client.generate(
+      text=input_text,
+      voice=selected_voice_name,
+      model="eleven_multilingual_v2"
+    )
+except Exception as e:
+    print(f"An error occurred: {e}")
+    # Handle the error or exit
+
+# Define the path to save the audio file, ensuring the directory exists
+# 1. Check if the directory for the audio file exists, and create it if it doesn't
+speech_file_directory = Path(__file__).parent / "audio-outputs"
+speech_file_directory.parent.mkdir(parents=True, exist_ok=True)
+
+# Define the path to the audio file, parameters for the speech model, and the manually typed input text for conversion to audio file
+speech_file_path = speech_file_directory / f"speech-{date_string}.mp3"
+
+# Save the response to a file
+with open(speech_file_path, 'wb') as out_file:
+    for chunk in response:
+        out_file.write(chunk)
+        
+print("\n")
+print("***************************************************")
+print(f" Generated Audio file saved to --> {speech_file_path}")
+print("***************************************************")
+print("\n")
+
+##########################################
+# *** End of Audio synthesis script 
+##########################################
+
+# Function to clean up old audio files
+def cleanup_files(directory, max_age=600):
+    """
+    Remove files in the specified directory that are older than max_age seconds.
+    """
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        # Check if the file is an actual file
+        if os.path.isfile(file_path):
+            # Check the file's modification time
+            if os.stat(file_path).st_mtime < time.time() - max_age:
+                try:
+                    os.remove(file_path)
+                    print("***************************************************")
+                    print(f"*** Deleted following old generated speech files --> {file_path}")
+                except Exception as e:
+                    print(f"Error while deleting file {file_path}: {e}")
+
+# You can call this function at the end of your Streamlit script
+# to clean up the audio files periodically (e.g., files older than 10 minutes)
+cleanup_files('./audio-outputs/', max_age=600)
+
+
